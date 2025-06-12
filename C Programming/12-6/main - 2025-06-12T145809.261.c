ce2aae5c3df5160350d60a92842b111caed8af29
Prakruti <109515514+prakruti95@users.noreply.github.com>
2025-06-12 14:59:34 +0530

    Add files via upload

---
+++ C Programming/12-6/main - 2025-06-12T145809.261.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    
+    printf("\n Your Name : Darshan");
+    printf("\t Your Age : 21");
+    printf("\t Your City : Rajkot");
+    
+    
+   getch(); 
+}
---
+++ C Programming/12-6/main - 2025-06-12T145813.669.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    
+    printf("\n Your Name : Darshan");
+    printf("\n Your Age : 21");
+    printf("\n Your City : Rajkot");
+    
+    
+   getch(); 
+}
